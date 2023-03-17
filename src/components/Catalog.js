6aa36354df0d0c5663888dcd3f3b6303720c7f69
Stanimira Rikova <stams_89@abv.bg>
2023-03-17 22:20:19 +0200

    shop products

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,81 +1,39 @@
+import { Details } from "./Details";
+
 export const Catalog = () => {
-    return (
-        <div className="container-fluid pt-5">
-            <div className="row px-xl-5 pb-3">
-                <div className="col-lg-4 col-md-6 pb-1">
-                    <div
-                        className="cat-item d-flex flex-column border mb-4"
-                        style={{ padding: 30 }}
-                    >
-
-                        <a href="" className="cat-img position-relative overflow-hidden mb-3">
-                            <img className="img-fluid" src="img/cat-1.jpg" alt="" />
-                        </a>
-                        <h5 className="font-weight-semi-bold m-0">Men's dresses</h5>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-6 pb-1">
-                    <div
-                        className="cat-item d-flex flex-column border mb-4"
-                        style={{ padding: 30 }}
-                    >
-                        
-                        <a href="" className="cat-img position-relative overflow-hidden mb-3">
-                            <img className="img-fluid" src="img/cat-2.jpg" alt="" />
-                        </a>
-                        <h5 className="font-weight-semi-bold m-0">Women's dresses</h5>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-6 pb-1">
-                    <div
-                        className="cat-item d-flex flex-column border mb-4"
-                        style={{ padding: 30 }}
-                    >
-                       
-                        <a href="" className="cat-img position-relative overflow-hidden mb-3">
-                            <img className="img-fluid" src="img/cat-3.jpg" alt="" />
-                        </a>
-                        <h5 className="font-weight-semi-bold m-0">Baby's dresses</h5>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-6 pb-1">
-                    <div
-                        className="cat-item d-flex flex-column border mb-4"
-                        style={{ padding: 30 }}
-                    >
-                      
-                        <a href="" className="cat-img position-relative overflow-hidden mb-3">
-                            <img className="img-fluid" src="img/cat-4.jpg" alt="" />
-                        </a>
-                        <h5 className="font-weight-semi-bold m-0">Accerssories</h5>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-6 pb-1">
-                    <div
-                        className="cat-item d-flex flex-column border mb-4"
-                        style={{ padding: 30 }}
-                    >
-                        
-                        <a href="" className="cat-img position-relative overflow-hidden mb-3">
-                            <img className="img-fluid" src="img/cat-5.jpg" alt="" />
-                        </a>
-                        <h5 className="font-weight-semi-bold m-0">Bags</h5>
-                    </div>
-                </div>
-                <div className="col-lg-4 col-md-6 pb-1">
-                    <div
-                        className="cat-item d-flex flex-column border mb-4"
-                        style={{ padding: 30 }}
-                    >
-                   
-                        <a href="" className="cat-img position-relative overflow-hidden mb-3">
-                            <img className="img-fluid" src="img/cat-6.jpg" alt="" />
-                        </a>
-                        <h5 className="font-weight-semi-bold m-0">Shoes</h5>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container-fluid pt-5">
+      <div className="text-center mb-4">
+        <h2 className="section-title px-5">
+          <span className="px-2">All Products</span>
+        </h2>
+      </div>
+      <div className="row px-xl-5 pb-3">
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+          < Details src="img/product-1.jpg" />
         </div>
-
-    )
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-2.jpg" />
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-3.jpg" />
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-4.jpg" />
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-5.jpg" />
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-6.jpg" />
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-7.jpg" />
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+        < Details src="img/product-8.jpg" />
+        </div>
+      </div>
+    </div>
+  )
 }
