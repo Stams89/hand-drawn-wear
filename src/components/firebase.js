1fa9c930c1b07d330a7b7e036cace80451bfe4bd
Stanimira Rikova <stams_89@abv.bg>
2024-01-10 14:51:58 +0200

    add firebase env variable

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,21 +2,19 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA48SYUt771pMRNTa4rULjm7glCSKabEbc",
-  authDomain: "hand-drawn-wear.firebaseapp.com",
-  projectId: "hand-drawn-wear",
-  storageBucket: "hand-drawn-wear.appspot.com",
-  messagingSenderId: "45927993869",
-  appId: "1:45927993869:web:fe154bcc954e76c82c3b44",
-  measurementId: "G-2WQ9VGZ28R"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
-
 const auth = firebase.auth();
 
 export { db, auth, firebaseConfig };
